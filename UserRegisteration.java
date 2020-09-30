d5428a488aebb0d98e1c07a5c0bfbae780d7b654
diksha-kalra <kalradiksha11@gmail.com>
2020-10-01 00:11:28 +0530

    Added: Printing Welcome Message

---
+++ UserRegisteration.java
@@ -0,0 +1,13 @@
+package com.blz.regex;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegisteration {
+	public static void main(String[] args) {
+	
+	//Printing Welcome Message
+	System.out.println("Welcome to User Registration Program");
+}
+}
