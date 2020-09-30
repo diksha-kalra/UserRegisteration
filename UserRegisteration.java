0de15ec0b57f014f94993ed51c1e0e5d27f3520e
diksha-kalra <kalradiksha11@gmail.com>
2020-10-01 00:55:11 +0530

    Added: Checking For Valid Email

--- UserRegisteration.java
+++ UserRegisteration.java
@@ -12,31 +12,45 @@ public class UserRegisteration {
 	
 	Scanner sc=new Scanner(System.in);
 	
+	//Valid first name
 	System.out.println("Enter The First Name");
 	String first_name=sc.next();
 	String pattern1 ="^[A-Z]{1}[a-z]{2,}$";
 	Pattern first_name_pattern = Pattern.compile(pattern1);
-    Matcher m = first_name_pattern.matcher(first_name);
-    if(m.matches()) {
-    	System.out.println("First Name follows pattern: "+first_name);
-    }
-    else {
-    	System.out.println("First Name does not follows the pattern");
-    	
-    }
+    	Matcher m = first_name_pattern.matcher(first_name);
+    	if(m.matches()) {
+    		System.out.println("First Name follows pattern: "+first_name);
+    	}
+    	else {
+    		System.out.println("First Name does not follows the pattern");	
+    	}
     
-    System.out.println("Enter The Last Name");
+    	//Valid last name
+    	System.out.println("Enter The Last Name");
 	String last_name=sc.next();
 	String pattern2 ="^[A-Z]{1}[a-z]{2,}$";
 	Pattern last_name_pattern = Pattern.compile(pattern2);
-    Matcher m2 = last_name_pattern.matcher(last_name);
-    if(m2.matches()) {
-    	System.out.println("Last Name follows pattern: "+last_name);
-    }
-    else {
-    	System.out.println("Last Name does not follows the pattern");
-    	
-    }
+   	Matcher m2 = last_name_pattern.matcher(last_name);
+    	if(m2.matches()) {
+    		System.out.println("Last Name follows pattern: "+last_name);
+    	}
+    	else {
+    		System.out.println("Last Name does not follows the pattern");
+    	}
+    
+    	//Valid email
+    	System.out.println("Enter The Email Id");
+	String email_id=sc.next();
+	String pattern3="^[abc]+(\\.[xyz]+)*@*+[bl]+[\\.[co]]*(\\.[A-Za-z]{2,})$";
+	Pattern email_pattern = Pattern.compile(pattern3);
+   	Matcher m3 = email_pattern.matcher(email_id);
+    	if(m3.matches()) {
+    		System.out.println("Email follows pattern: "+email_id);
+    	}
+    	else {
+    		System.out.println("Email does not follows the pattern");
+    	}
+
 
     	sc.close();
 }
