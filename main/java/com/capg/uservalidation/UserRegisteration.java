1e6f87cdd7ece0d33ddd295286b0d38a44bff60d
diksha-kalra <kalradiksha11@gmail.com>
2020-10-07 00:36:18 +0530

    Refactor: custom exception for user validation

--- main/java/com/capg/uservalidation/UserRegisteration.java
+++ main/java/com/capg/uservalidation/UserRegisteration.java
@@ -1,4 +1,5 @@
 package com.capg.uservalidation;
+
 import java.util.regex.Pattern;
 
 public class UserRegisteration {
@@ -6,7 +7,7 @@ public class UserRegisteration {
 	private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 	private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9]+([._+-][0-9a-zA-Z])*@*+[a-zA-Z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-	private static final String PHONE_NUMBER_PATTERN = "^[1-9]{2}\\s[1-9]{1}[0-9]{9}$";
+	private static final String PHONE_NUMBER_PATTERN = "^[1-9]{2}[\\s][1-9]{1}[0-9]{9}$";
 	private static final String PASSWORD_PATTERN = "^(?=.*[@#$%!|'<>.^*()%!])(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])[^\\s]{8,}$";
 	private static final String EMAIL_SAMPLES_PATTERN = "^[a-zA-Z]+((\\.[0-9]+)|(\\+[0-9]+)|(\\-[0-9]+)|([0-9]))*@*+[a-zA-Z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 
--- test/java/com/capg/uservalidation/UserRegisterationTest.java
+++ test/java/com/capg/uservalidation/UserRegisterationTest.java
@@ -3,123 +3,120 @@ package com.capg.uservalidation;
 import org.junit.Test;
 import org.junit.Assert;
 
-
 public class UserRegisterationTest {
 
-   	
-	@Test 
-   	public void givenFirstName_WhenProper_ShouldReturnTrue() {
-		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validateFirstName("Diksha");
-		Assert.assertEquals(true,result);	
-    	}
-    
-    	@Test
-    	public void givenFirstName_WhenImproper_ShouldThrowException() throws UserValidationException {
-    		UserRegisteration validator = new UserRegisteration();
-    		boolean result= validator.validateFirstName("Di ");
-			try {
-				if(result==true) {
-					System.out.println("Matches pattern");
-				}
-				else {
-					throw new UserValidationException(UserValidationException.ExceptionType.INVALID_FIRST_NAME,"invalid first name");
-				}
-			}catch(UserValidationException e){
-				System.out.println("e-"+e);
-				}
-    	}
-    
-    	@Test
-    	public void givenLastName_WhenProper_ShouldReturnTrue() {
-    		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validateLasttName("Kalra");
-		Assert.assertEquals(true,result);	
-    	}
-    
-    	@Test
-    	public void givenLastName_WhenImproper_ShouldThrowException() throws UserValidationException {
-    		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validateLasttName("ka ");
-			try {
-				if(result==true) {
-					System.out.println("Matches pattern");
-				}
-				else {
-					throw new UserValidationException(UserValidationException.ExceptionType.INVALID_LAST_NAME,"invalid last name");
-				}
-			}catch(UserValidationException e){
-				System.out.println("e-"+e);
-			}	
-    }
-    
-    	@Test
-    	public void givenEmail_WhenProper_ShouldReturnTrue() {
-    		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validateEmail("Kalradiksha11@gmail.com");
-		Assert.assertEquals(true,result);	
-    	}
-    
 	@Test
-    	public void givenEmail_WhenImproper_ShouldThrowException()throws UserValidationException  {
+	public void givenFirstName_WhenProper_ShouldReturnTrue() {
 		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validateEmail("diksha@.com");
-			try {
-				if(result==true) {
-					System.out.println("Matches pattern");
-				}
-				else {
-					throw new UserValidationException(UserValidationException.ExceptionType.INVALID_EMAIL,"invalid email");
-				}
-			}catch(UserValidationException e){
-				System.out.println("e-"+e);
-			}
-    }
-    	
-	@Test
-    	public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
-		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validatePhoneNumber("91 9899151937");
-		Assert.assertEquals(true,result);	
-    	}
-    
-	@Test
-    	public void givenPhoneNumber_WhenImproper_ShouldThrowException()throws UserValidationException  {
-		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validatePhoneNumber("9899151937");
-			try {
-				if(result==true) {
-					System.out.println("Matches pattern");
-				}
-				else {
-					throw new UserValidationException(UserValidationException.ExceptionType.INVALID_NUMBER,"invalid phone number");
-				}
-			}catch(UserValidationException e){
-				System.out.println("e-"+e);
-			}	
-    }
-	
-    	@Test
-    	public void givenPassword_WhenProper_ShouldReturnTrue() {
-    		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validatePassword("Ikdn@1234");
-		Assert.assertEquals(true,result);	
-    	}
-    
-	@Test
-    	public void givenPassword_WhenImproper_ShouldThrowException()throws UserValidationException  {
-		UserRegisteration validator = new UserRegisteration();
-		boolean result= validator.validatePassword("Diksha123");
-			try {
-				if(result==true) {
-					System.out.println("Matches pattern");
-				}
-				else {
-					throw new UserValidationException(UserValidationException.ExceptionType.INVALID_PASSWORD,"invalid password");
-				}
-			}catch(UserValidationException e){
-				System.out.println("e-"+e);
-			}		
-    }
+		boolean result = validator.validateFirstName("Diksha");
+		Assert.assertEquals(true, result);
+	}
 
+	@Test
+	public void givenFirstName_WhenImproper_ShouldThrowException() throws UserRegisterationException {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validateFirstName("Di ");
+		try {
+			if (result == true) {
+				System.out.println("Matches pattern");
+			} else {
+				throw new UserRegisterationException(UserRegisterationException.ExceptionType.INVALID_FIRST_NAME,
+						"invalid first name");
+			}
+		} catch (UserRegisterationException e) {
+			System.out.println("e-" + e);
+		}
+	}
+
+	@Test
+	public void givenLastName_WhenProper_ShouldReturnTrue() {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validateLasttName("Kalra");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenLastName_WhenImproper_ShouldThrowException() throws UserRegisterationException {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validateLasttName("ka ");
+		try {
+			if (result == true) {
+				System.out.println("Matches pattern");
+			} else {
+				throw new UserRegisterationException(UserRegisterationException.ExceptionType.INVALID_LAST_NAME,
+						"invalid last name");
+			}
+		} catch (UserRegisterationException e) {
+			System.out.println("e-" + e);
+		}
+	}
+
+	@Test
+	public void givenEmail_WhenProper_ShouldReturnTrue() {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validateEmail("Kalradiksha11@gmail.com");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenEmail_WhenImproper_ShouldThrowException() throws UserRegisterationException {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validateEmail("diksha@.com");
+		try {
+			if (result == true) {
+				System.out.println("Matches pattern");
+			} else {
+				throw new UserRegisterationException(UserRegisterationException.ExceptionType.INVALID_EMAIL,
+						"invalid email");
+			}
+		} catch (UserRegisterationException e) {
+			System.out.println("e-" + e);
+		}
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenProper_ShouldReturnTrue() {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validatePhoneNumber("91 9899151937");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPhoneNumber_WhenImproper_ShouldThrowException() throws UserRegisterationException {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validatePhoneNumber("9899151937");
+		try {
+			if (result == true) {
+				System.out.println("Matches pattern");
+			} else {
+				throw new UserRegisterationException(UserRegisterationException.ExceptionType.INVALID_NUMBER,
+						"invalid phone number");
+			}
+		} catch (UserRegisterationException e) {
+			System.out.println("e-" + e);
+		}
+	}
+
+	@Test
+	public void givenPassword_WhenProper_ShouldReturnTrue() {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validatePassword("Ikdn@1234");
+		Assert.assertEquals(true, result);
+	}
+
+	@Test
+	public void givenPassword_WhenImproper_ShouldThrowException() throws UserRegisterationException {
+		UserRegisteration validator = new UserRegisteration();
+		boolean result = validator.validatePassword("Diksha123");
+		try {
+			if (result == true) {
+				System.out.println("Matches pattern");
+			} else {
+				throw new UserRegisterationException(UserRegisterationException.ExceptionType.INVALID_PASSWORD,
+						"invalid password");
+			}
+		} catch (UserRegisterationException e) {
+			System.out.println("e-" + e);
+		}
+	}
 }
